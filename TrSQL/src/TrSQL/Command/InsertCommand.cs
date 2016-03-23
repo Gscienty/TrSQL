3db2dd5241787f155142f0e13c1fc70e8a0711f2
gscienty <gaoxiaochuan@hotmail.com>
2016-03-24 00:48:15 +0800

    完善抽象数据库类，添加基础插入语句类

---
+++ TrSQL/src/TrSQL/Command/InsertCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace TrSQL.Command
+{
+    public class InsertCommand : AbstractSQL
+    {
+        #region 字段
+        private List<Items.DataParameter> _insertFields;
+        #endregion
+
+        #region 属性
+        /// <summary>
+        /// SQL语句类型
+        /// </summary>
+        public override SqlDbType SQLType
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #endregion
+
+        #region 构造方法
+        /// <summary>
+        /// 初始化插入语句类
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="tableName"></param>
+        internal InsertCommand(AbstractDatabase database, String tableName) : base(database)
+        {
+            this._insertFields = new List<Items.DataParameter>();
+        }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 获取SQL语句字符串
+        /// </summary>
+        /// <returns></returns>
+        public override string GetSQLText()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 转换为数据库命令
+        /// </summary>
+        /// <returns></returns>
+        public override IDbCommand ToDbCommand()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
--- TrSQL/src/TrSQL/IDatabase.cs
+++ TrSQL/src/TrSQL/IDatabase.cs
@@ -2,48 +2,49 @@
 using TrSQL.Define;
 using System.Data;
 using System.Data.Common;
+
 namespace TrSQL
 {
-    public abstract class AbstractDatabase : IDatabase
+    /// <summary>
+    /// 数据库接口
+    /// </summary>
+    public interface IDatabase
     {
-        #region 字段
-        private DbProviderFactory _dbProvider = null;
-        #endregion
-
         #region 属性
         /// <summary>
-        /// 获取当前数据库的类型
+        /// 获取当前数据库类型
         /// </summary>
-        public abstract DatabaseType DatabaseType { get; }
+        DatabaseType DatabaseType { get; }
 
         /// <summary>
         /// 获取当前数据库提供者名称
         /// </summary>
-        public string ProviderName { get { return this._dbProvider.GetType().ToString(); } }
+        string ProviderName { get; }
 
         /// <summary>
-        /// 连接字符串
+        /// 获取当前数据库连接字符串
         /// </summary>
-        public string ConnectionString { get; private set; } = null;
+        string ConnectionString { get; }
         #endregion
 
         #region 方法
         /// <summary>
-        /// 获取当前数据库对应的数据参数工厂
+        /// 数据库参数工厂
         /// </summary>
-        public abstract IDataParameter DataParameterFactory();
+        /// <returns>数据库参数</returns>
+        IDataParameter DataParameterFactory();
 
         /// <summary>
         /// 数据库命令工厂
         /// </summary>
         /// <returns>数据库命令</returns>
-        public abstract IDbCommand CommandFactory();
+        IDbCommand CommandFactory();
 
         /// <summary>
         /// 数据库事务工厂
         /// </summary>
         /// <returns>数据库事务</returns>
-        public abstract IDbConnection TransactionFactory();
+        IDbConnection TransactionFactory();
         #endregion
 
 
